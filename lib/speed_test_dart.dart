deb880e4ef2fb725b684f363a84847d0098d137e
git-elliot <fs0c19ty@protonmail.com>
2025-03-04 21:20:25 +0530

    Update speed_test_dart.dart

--- lib/speed_test_dart.dart
+++ lib/speed_test_dart.dart
@@ -13,8 +13,9 @@ import 'package:xml/xml.dart';
 /// A Speed tester.
 class SpeedTestDart {
   /// Returns [Settings] from speedtest.net.
-  Future<Settings> getSettings() async {
-    final response = await http.get(Uri.parse(configUrl));
+  Future<Settings> getSettings(Map<String, String>? headers) async {
+    final response = await http
+        .get(Uri.parse(configUrl), headers: headers);
     final settings = Settings.fromXMLElement(
       XmlDocument.parse(response.body).getElement('settings'),
     );
